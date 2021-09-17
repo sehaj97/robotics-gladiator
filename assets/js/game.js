53e1572bd89ff4d59a4ccc07b32084352769bcf2
sehaj97 <sehajmagan@gmail.com>
2021-09-16 20:36:37 -0400

    added loop for multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,12 @@ var playerAttack = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 var enemyHealth = 50;
 var enemyAttack = 10;
 
@@ -65,5 +69,3 @@ var fight = function() {
     window.alert("You need to choose a valid option. Try again!");
   }
 }
-
-fight();
